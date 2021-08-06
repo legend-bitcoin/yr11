95c7ba5ae6d980327a9a049e479c46aab3181a90
James Poon <JPoon@stu.kerikerihigh.ac.nz>
2021-08-06 12:15:21 +1200

    edit

--- whoisinspace.py
+++ whoisinspace.py
@@ -2,5 +2,5 @@ import requests
 
 response= requests.get("https://opentdb.com/api.php?amount=10&category=21&difficulty=easy")
 
-for astronaut in response.json()[people]:
+for astronaut in response.json()[]:
     print('name', astronaut['name'],'aga',astronaut['age'])
